05ccfc0829e6f7c8d1e269e484bbd26a2251ca2b
yiuf <cjcjcj@mail.ustc.edu.cn>
2024-12-24 14:18:58 +0800

    实在难顶

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,26 @@
 ﻿#include <QDebug>
+#include <QtCore/QCoreApplication>
+#include <QtWebEngineQuick/qtwebenginequickglobal.h>
 
-int main(){
+#include <QtQml/QQmlApplicationEngine>
+
+#include <QtGui/QGuiApplication>
+int main(int argc, char **argv){
     qDebug()<<"hello world";
+
+    QCoreApplication::setApplicationName("QMemos");
+    QtWebEngineQuick::initialize();
+
+    QGuiApplication app(argc, argv);
+
+
+    QQmlApplicationEngine appEngine;
+    appEngine.load(QUrl("qrc:/MemosPage.qml"));
+
+    if (appEngine.rootObjects().isEmpty())
+        qFatal("Failed to load sources");
+
+
+
+    return app.exec();
 }
